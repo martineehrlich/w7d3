71af0c808798c6e1e42bc471fc0576e8b1a8ed1e
App Academy Student <student@appacademy.io>
2015-08-05 16:27:09 -0700

    add delete button to PostShow

--- JournalApp/app/assets/javascripts/views/post_show.js
+++ JournalApp/app/assets/javascripts/views/post_show.js
@@ -3,12 +3,21 @@ JournalApp.Views.PostShow = Backbone.View.extend({
     this.listenTo(this.model, "sync", this.render);
   },
 
+  events: {
+    "click button.delete-post": 'deletePost'
+  },
+
   template: JST["post_show"],
 
   render: function () {
     var content = this.template({post: this.model});
     this.$el.html(content);
     return this;
-  }
+  },
 
+  deletePost: function(event) {
+    event.preventDefault();
+    this.model.destroy();
+    Backbone.history.navigate("", { trigger: true });
+  }
 });
