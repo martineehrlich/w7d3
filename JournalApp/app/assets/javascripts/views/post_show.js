87cb2801330f4e17a180ee9e129f785c1e9159f4
App Academy Student <student@appacademy.io>
2015-08-05 16:56:14 -0700

    add edit-in-place functionality to show page

--- JournalApp/app/assets/javascripts/views/post_show.js
+++ JournalApp/app/assets/javascripts/views/post_show.js
@@ -4,7 +4,9 @@ JournalApp.Views.PostShow = Backbone.View.extend({
   },
 
   events: {
-    "click button.delete-post": 'deletePost'
+    "click button.delete-post": 'deletePost',
+    'dblclick .post-content': 'showInputBox',
+    'blur .input-box': "saveWithBlur"
   },
 
   template: JST["post_show"],
@@ -15,9 +17,30 @@ JournalApp.Views.PostShow = Backbone.View.extend({
     return this;
   },
 
-  deletePost: function(event) {
+  deletePost: function (event) {
     event.preventDefault();
     this.model.destroy();
     Backbone.history.navigate("", { trigger: true });
+  },
+
+  showInputBox: function (event) {
+    event.preventDefault(); // ???
+    var inputTemplate = JST["_input_box"];
+    var $targetEl = $(event.currentTarget);
+    var prevContent = $targetEl.text();
+    $targetEl.html(inputTemplate({content: prevContent}));
+  },
+
+  saveWithBlur: function (event) {
+    event.preventDefault();
+    var $targetEl = $(event.currentTarget);
+    var $parentEl = $targetEl.parent();
+    var updatedAttr = $targetEl.val();
+
+    $parentEl.html(updatedAttr);
+    var attr = $parentEl.data("attrname");
+
+    this.model.set(attr, updatedAttr);
+    this.model.save({ attr: updatedAttr }, {});
   }
 });
